c9fc00195dc5e87d5c2a7fe850a0f1d8dce4c0b3
Marcus Bronstein <>
2020-04-20 22:21:28 +0100

    feat(Hungarian): working for n x n
    working for quadratic matrices

--- algorithms/hungarian.go
+++ algorithms/hungarian.go
@@ -1,9 +1,241 @@
 package algorithms
 
+import (
+	"math"
+)
+
+func arrayCopy(matrix [][]float64) [][]float64 {
+	n := len(matrix)
+	m := len(matrix[0])
+	duplicate := make([][]float64, n)
+	data := make([]float64, n*m)
+	for i := range matrix {
+		start := i * m
+		end := start + m
+		duplicate[i] = data[start:end:end]
+		copy(duplicate[i], matrix[i])
+	}
+	return duplicate
+
+}
+
+func zeroOutMatrix(input [][]float64) [][]float64 {
+	for row := range input {
+		for col := range input {
+			input[row][col] = 0
+		}
+	}
+	return input
+}
+
 // Solve receives an MxN matrix and returns a with the chosen row for each column.
+func Solve(input [][]float64) (float64, []int) {
+
+	var total float64
+	local := arrayCopy(input)
+	result := make([]int, len(input))
+	local = zeroRows(local)
+	local = zeroColumns(local)
+	marked := makeArray(len(input))
+	var markedColumns map[int]bool
+	var markedRows map[int]bool
+	marked, markedColumns = rowScanning(local, marked)
+	marked, markedRows = colScanning(local, marked)
+	for checkLines(marked) != len(input) {
+		local = step4(local, markedRows, markedColumns)
+		marked = zeroOutMatrix(marked)
+		marked, markedColumns = rowScanning(local, marked)
+		marked, markedRows = colScanning(local, marked)
+	}
+
+	for row := range input {
+		for col := range input {
+			if marked[row][col] == 1 {
+
+				total += input[row][col]
+				result[row] = col
+			}
+
+		}
+	}
+	return total, result
+}
+
+func makeArray(dimension int) [][]float64 {
+	a := make([][]float64, dimension)
+	for i := range a {
+		a[i] = make([]float64, dimension)
+	}
+	return a
+}
+
+// checkLines check how many lines are covered
+func checkLines(marked [][]float64) int {
+	total := 0
+	for row := range marked {
+		for col := range marked {
+			if marked[row][col] == 1 {
+				total++
+			}
+		}
+	}
+	return total
+}
+
+func step4(input [][]float64, rows map[int]bool, columns map[int]bool) [][]float64 {
+	local := input
+	// calculate min of the not cover by lines
+	min := math.MaxFloat64
+	for row := range input {
+		for col := range input {
+			if !rows[row] && !columns[col] {
+				if input[row][col] < min {
+					min = input[row][col]
+				}
+			}
+		}
+	}
+
+	for row := range input {
+		for col := range input {
+			if !rows[row] && !columns[col] {
+				local[row][col] = input[row][col] - min
+			}
+			if rows[row] && columns[col] {
+				local[row][col] = input[row][col] + min
+			}
+		}
+	}
+	return local
+}
+
+//colScanning return the marked array (1 assigned -1 cross out) and the crossed out rows
+//col scan mark the col with one zeros and cross out its rows
+func colScanning(input [][]float64, marked [][]float64) ([][]float64, map[int]bool) {
+	markedRow := make(map[int]bool)
+	for col := range input {
+		chosenRow := -1
+		for row := range input {
+			if input[row][col] == 0 && marked[row][col] == 0 {
+				if chosenRow >= 0 { // already had another zero
+					chosenRow = -1
+					break
+				}
+				chosenRow = row
+			}
+		}
+		if chosenRow >= 0 {
+			marked[chosenRow][col] = 1
+			markedRow[chosenRow] = true
+			for col2 := range input {
+
+				if input[chosenRow][col2] == 0 && col != col2 {
+					marked[chosenRow][col2] = -1
+				}
+
+			}
+		}
+	}
+	return marked, markedRow
+}
+
+//rowScanning return the marked array (1 assigned -1 cross out) and the crossed out columns
+//row scan mark the row with one zeros and cross out its colums
+func rowScanning(input [][]float64, marked [][]float64) ([][]float64, map[int]bool) {
+	markedCol := make(map[int]bool)
+	for row := range input {
+		chosenCol := -1
+		for col := range input {
+			if input[row][col] == 0 && marked[row][col] == 0 {
+				if chosenCol >= 0 { // already had another zero
+					chosenCol = -1
+					break
+				}
+				chosenCol = col
+			}
+		}
+		if chosenCol >= 0 {
+			marked[row][chosenCol] = 1
+			markedCol[chosenCol] = true
+			for row2 := range input {
+
+				if input[row2][chosenCol] == 0 && row != row2 {
+					marked[row2][chosenCol] = -1
+				}
+
+			}
+		}
+	}
+	return marked, markedCol
+}
+
+// cover all the zeros with the minimum possible number of lines
+func coverZeros(input [][]float64) int {
+
+	return 0
+}
+func calculateNumberOfLines(markedColumns map[int]bool, markedRows map[int]bool, dimension int) int {
+	result := len(markedColumns)
+	for i := 0; i < dimension; i++ {
+		if !markedRows[i] {
+			result++
+		}
+	}
+	return result
+}
+
+func zeroRows(input [][]float64) [][]float64 {
+	localMatrix := input
+	minValues := make([]float64, len(localMatrix))
+
+	// find min for each row
+	for col := range localMatrix[0] { // go through columns
+		for row := range localMatrix { // for each row
+			if col == 0 { // first column
+				minValues[row] = localMatrix[row][0]
+			}
+			if minValues[row] > localMatrix[row][col] {
+				minValues[row] = localMatrix[row][col]
+			}
+		}
+	}
+
+	// now subtract all values of the row by the min
+	for row := range localMatrix {
+		for col := range localMatrix[row] { // go through columns
+			localMatrix[row][col] = localMatrix[row][col] - minValues[row]
+		}
+	}
+	return localMatrix
+}
+
+func zeroColumns(input [][]float64) [][]float64 {
+	localMatrix := input
+	minValues := make([]float64, len(localMatrix))
+
+	// find min for each column
+	for col := range localMatrix[0] { // go through columns
+		for row := range localMatrix { // for each row
+			if row == 0 { // first column
+				minValues[col] = localMatrix[0][col]
+			}
+			if minValues[col] > localMatrix[row][col] {
+				minValues[col] = localMatrix[row][col]
+			}
+		}
+	}
+
+	// now subtract all values of the column by the min
+	for row := range localMatrix {
+		for col := range localMatrix[row] { // go through columns
+			localMatrix[row][col] = localMatrix[row][col] - minValues[col]
+		}
+	}
+	return localMatrix
+}
 
 //normalize receives and MxN matrix and makes it max(m,n) matrix
-func normalizeMatrix(originalMatrix [][]int32) [][]int32 {
+func normalizeMatrix(originalMatrix [][]float64) [][]float64 {
 	localMatrix := originalMatrix
 	// assuming there are missing columns
 	maxDimension := len(originalMatrix) //total rows
@@ -11,16 +243,16 @@ func normalizeMatrix(originalMatrix [][]int32) [][]int32 {
 	if maxDimension < len(originalMatrix[0]) {
 		//there are missing rows
 		maxDimension = len(originalMatrix[0]) // total columns
-		maxValues := make([]int32, maxDimension)
+		maxValues := make([]float64, maxDimension)
 
 		// find maxValue per column
-		for i := range originalMatrix { // go through rows
-			for j := range originalMatrix[i] { // go through columns
-				if i == 0 { // first line
-					maxValues[j] = originalMatrix[0][j]
+		for row := range originalMatrix { // go through rows
+			for col := range originalMatrix[row] { // go through columns
+				if row == 0 { // first line
+					maxValues[col] = originalMatrix[0][col]
 				}
-				if maxValues[j] < originalMatrix[i][j] {
-					maxValues[j] = originalMatrix[i][j]
+				if maxValues[col] < originalMatrix[row][col] {
+					maxValues[col] = originalMatrix[row][col]
 				}
 			}
 		}
@@ -30,7 +262,7 @@ func normalizeMatrix(originalMatrix [][]int32) [][]int32 {
 		}
 		return localMatrix
 	}
-	maxValues := make([]int32, maxDimension)
+	maxValues := make([]float64, maxDimension)
 	// there are missing columns
 
 	for col := range originalMatrix[0] { // go through columns
--- algorithms/hungarian_test.go
+++ algorithms/hungarian_test.go
@@ -2,20 +2,140 @@ package algorithms
 
 import "testing"
 
+var testSolve = []struct {
+	name   string
+	data   [][]float64
+	result []int
+	total  float64
+}{
+	{
+		name: "test 1",
+		data: [][]float64{
+			{9, 11, 14, 11, 7},
+			{6, 15, 13, 13, 10},
+			{12, 13, 6, 8, 8},
+			{11, 9, 10, 12, 9},
+			{7, 12, 14, 10, 14},
+		},
+		result: []int{4, 0, 2, 1, 3},
+		total:  38,
+	},
+	{
+		name: "test 2",
+		data: [][]float64{
+			{108, 125, 150},
+			{150, 135, 175},
+			{122, 148, 250},
+		},
+		result: []int{2, 1, 0},
+		total:  407,
+	},
+}
+
+var testStep4 = []struct {
+	name    string
+	data    [][]float64
+	rows    map[int]bool
+	columns map[int]bool
+	result  [][]float64
+}{
+	{
+		name: "test 1",
+		data: [][]float64{
+			{2, 4, 7, 2, 0},
+			{0, 9, 7, 5, 4},
+			{6, 7, 0, 0, 2},
+			{2, 0, 1, 1, 0},
+			{0, 5, 7, 1, 7},
+		},
+		rows:    map[int]bool{2: true},
+		columns: map[int]bool{0: true, 1: true, 4: true},
+		result: [][]float64{
+			{2, 4, 6, 1, 0},
+			{0, 9, 6, 4, 4},
+			{7, 8, 0, 0, 3},
+			{2, 0, 0, 0, 0},
+			{0, 5, 6, 0, 7},
+		},
+	},
+}
+
+var testColScanning = []struct {
+	name   string
+	data   [][]float64
+	marked [][]float64
+	result [][]float64
+}{
+	{
+		name: "test 1",
+		data: [][]float64{
+			{2, 4, 7, 2, 0},
+			{0, 9, 7, 5, 4},
+			{6, 7, 0, 0, 2},
+			{2, 0, 1, 1, 0},
+			{0, 5, 7, 1, 7},
+		},
+		marked: [][]float64{
+			{0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 1, 0, 0, -1},
+			{-1, 0, 0, 0, 0},
+		},
+		result: [][]float64{
+			{0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0},
+			{0, 0, 1, -1, 0},
+			{0, 1, 0, 0, -1},
+			{-1, 0, 0, 0, 0},
+		}},
+}
+
+var testRowScanning = []struct {
+	name   string
+	data   [][]float64
+	marked [][]float64
+	result [][]float64
+}{
+	{
+		name: "test 1",
+		data: [][]float64{
+			{2, 4, 7, 2, 0},
+			{0, 9, 7, 5, 4},
+			{6, 7, 0, 0, 2},
+			{2, 0, 1, 1, 0},
+			{0, 5, 7, 1, 7},
+		},
+		marked: [][]float64{
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+		},
+		result: [][]float64{
+			{0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 1, 0, 0, -1},
+			{-1, 0, 0, 0, 0},
+		}},
+}
+
 var testsNormalize = []struct {
 	name   string
-	data   [][]int32
-	result [][]int32
+	data   [][]float64
+	result [][]float64
 }{
 	{
 		name: "missing 1 row",
-		data: [][]int32{
+		data: [][]float64{
 			{6, 2, 3, 4, 5},
 			{3, 8, 2, 8, 1},
 			{9, 9, 5, 4, 2},
 			{6, 7, 3, 4, 3},
 		},
-		result: [][]int32{
+		result: [][]float64{
 			{6, 2, 3, 4, 5},
 			{3, 8, 2, 8, 1},
 			{9, 9, 5, 4, 2},
@@ -23,14 +143,14 @@ var testsNormalize = []struct {
 			{9, 9, 5, 8, 5},
 		}},
 	{name: "missing 1 column",
-		data: [][]int32{
+		data: [][]float64{
 			{6, 2, 3, 4},
 			{3, 8, 2, 8},
 			{9, 9, 5, 4},
 			{6, 7, 3, 4},
 			{9, 9, 5, 8},
 		},
-		result: [][]int32{
+		result: [][]float64{
 			{6, 2, 3, 4, 6},
 			{3, 8, 2, 8, 8},
 			{9, 9, 5, 4, 9},
@@ -38,20 +158,93 @@ var testsNormalize = []struct {
 			{9, 9, 5, 8, 9},
 		}},
 	{name: "missing 2 columns",
-		data: [][]int32{
+		data: [][]float64{
 			{6, 2, 3},
 			{3, 8, 2},
 			{9, 9, 5},
 			{6, 7, 3},
 			{9, 9, 5},
 		},
-		result: [][]int32{
+		result: [][]float64{
 			{6, 2, 3, 6, 6},
 			{3, 8, 2, 8, 8},
 			{9, 9, 5, 9, 9},
 			{6, 7, 3, 7, 7},
 			{9, 9, 5, 9, 9},
 		}},
+}
+
+var testsZeroRows = []struct {
+	name   string
+	data   [][]float64
+	result [][]float64
+}{
+	{
+		name: "test 1",
+		data: [][]float64{
+			{6, 2, 3, 4, 5},
+			{3, 8, 2, 8, 1},
+			{9, 9, 5, 4, 2},
+			{6, 7, 3, 4, 3},
+			{9, 9, 5, 8, 5},
+		},
+		result: [][]float64{
+			{4, 0, 1, 2, 3},
+			{2, 7, 1, 7, 0},
+			{7, 7, 3, 2, 0},
+			{3, 4, 0, 1, 0},
+			{4, 4, 0, 3, 0},
+		},
+	},
+}
+
+var testsZeroCols = []struct {
+	name   string
+	data   [][]float64
+	result [][]float64
+}{
+	{
+		name: "test 1",
+		data: [][]float64{
+			{6, 2, 3, 4, 5},
+			{3, 8, 2, 8, 1},
+			{9, 9, 5, 4, 2},
+			{6, 7, 3, 4, 3},
+			{9, 9, 5, 8, 5},
+		},
+		result: [][]float64{
+			{3, 0, 1, 0, 4},
+			{0, 6, 0, 4, 0},
+			{6, 7, 3, 0, 1},
+			{3, 5, 1, 0, 2},
+			{6, 7, 3, 4, 4},
+		},
+	},
+}
+
+func TestCheckLines(t *testing.T) {
+	data := [][]float64{
+		{0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0},
+		{0, 0, 1, -1, 0},
+		{0, 1, 0, 0, -1},
+		{-1, 0, 0, 0, 0},
+	}
+
+	result := checkLines(data)
+	if result != 4 {
+		t.Errorf("TestCheckLines want %v got %v", 4, result)
+	}
+
+}
+
+func TestStep4(t *testing.T) {
+	for _, testToBeDone := range testStep4 {
+		result := step4(testToBeDone.data, testToBeDone.rows, testToBeDone.columns)
+		if !compareMulti(result, testToBeDone.result) {
+			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.result, result)
+		}
+	}
 }
 
 func TestNormalizeMatrix(t *testing.T) {
@@ -61,10 +254,68 @@ func TestNormalizeMatrix(t *testing.T) {
 			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.result, result)
 		}
 	}
-
 }
 
-func compareMulti(m [][]int32, n [][]int32) bool {
+func TestRowScanning(t *testing.T) {
+	for _, testToBeDone := range testRowScanning {
+		result, _ := rowScanning(testToBeDone.data, testToBeDone.marked)
+		if !compareMulti(result, testToBeDone.result) {
+			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.result, result)
+		}
+	}
+}
+
+func TestSolve(t *testing.T) {
+	for _, testToBeDone := range testSolve {
+		total, columns := Solve(testToBeDone.data)
+		if !compareSimple(columns, testToBeDone.result) {
+			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.result, columns)
+		}
+		if total != testToBeDone.total {
+			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.total, total)
+		}
+	}
+}
+
+func TestColScanning(t *testing.T) {
+	for _, testToBeDone := range testColScanning {
+		result, _ := colScanning(testToBeDone.data, testToBeDone.marked)
+		if !compareMulti(result, testToBeDone.result) {
+			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.result, result)
+		}
+	}
+}
+
+func TestZeroRows(t *testing.T) {
+	for _, testToBeDone := range testsZeroRows {
+		result := zeroRows(testToBeDone.data)
+		if !compareMulti(result, testToBeDone.result) {
+			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.result, result)
+		}
+	}
+}
+
+func TestZeroColumns(t *testing.T) {
+	for _, testToBeDone := range testsZeroCols {
+		result := zeroColumns(testToBeDone.data)
+		if !compareMulti(result, testToBeDone.result) {
+			t.Errorf("%s want %v got %v", testToBeDone.name, testToBeDone.result, result)
+		}
+	}
+}
+func compareSimple(m []int, n []int) bool {
+	if len(m) != len(n) {
+		return false
+	}
+	for i := range m {
+		if m[i] != n[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func compareMulti(m [][]float64, n [][]float64) bool {
 	if len(m) != len(n) || len(m[0]) != len(n[0]) {
 		return false
 	}
